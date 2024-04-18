67cd4b363f95c0cc72300ed872d70e8bbe727a57
unknown <zanbolotov.daniel2004@gmail.com>
2024-04-18 20:33:25 +0600

    dfd

--- main.js
+++ main.js
@@ -35,3 +35,31 @@ function stopScroll() {
   var scrollingText = document.querySelector('.scrolling-text p');
   scrollingText.style.animationPlayState = 'paused';
 }
+
+
+
+// ---------------------------------------------------------
+
+document.addEventListener("DOMContentLoaded", function() {
+  const slides = document.querySelectorAll(".slide");
+  let currentIndex = 0;
+
+  function showSlide(index) {
+      slides.forEach((slide, i) => {
+          slide.classList.remove("active");
+          if (i === index) {
+              slide.classList.add("active");
+          }
+      });
+  }
+
+  document.querySelector(".prev").addEventListener("click", function() {
+      currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+      showSlide(currentIndex);
+  });
+
+  document.querySelector(".next").addEventListener("click", function() {
+      currentIndex = (currentIndex + 1) % slides.length;
+      showSlide(currentIndex);
+  });
+});
